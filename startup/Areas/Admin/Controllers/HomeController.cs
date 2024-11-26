319503cb26d0bbb7145f54e2850ae833a35881bd
quanzz123 <nguyenquandc37@gmail.com>
2024-11-26 18:19:26 +0700

    logout

--- startup/Areas/Admin/Controllers/HomeController.cs
+++ startup/Areas/Admin/Controllers/HomeController.cs
@@ -15,5 +15,14 @@ namespace startup.Areas.Admin.Controllers
             }
             return View();
         }
+        public IActionResult Logout()
+        {
+            Functions._UserID = 0;
+            Functions._UserName = string.Empty;
+            Functions._Email = string.Empty;
+            Functions._Message = string.Empty;
+            Functions._MessageEmail = string.Empty;
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
